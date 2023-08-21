456a941c6c83f31ab778d3b173f251e0a117da28
tx7do <yanglinbo@gmail.com>
2023-08-21 23:38:33 +0800

    feat: fix some post method upload function bug.

--- react-app/src/util/post_upload.ts
+++ react-app/src/util/post_upload.ts
@@ -3,10 +3,11 @@ import axios from 'axios';
 export class PostFile {
   static xhr(file: File, url: string, data: object) {
     const formData = new FormData();
+    Object.entries(data)
+      .forEach(([k, v]) => {
+        formData.append(k, v);
+      });
     formData.append('file', file);
-    Object.entries(data).forEach(([k, v]) => {
-      formData.append(k, v);
-    });
 
     const xhr = new XMLHttpRequest();
     xhr.open('POST', url, true);
@@ -14,7 +15,7 @@ export class PostFile {
 
     xhr.onload = () => {
       if (xhr.status === 200 || xhr.status === 204) {
-        console.log(`[${xhr.status}] ${file.name} 上传成功`);
+        console.info(`[${xhr.status}] ${file.name} 上传成功`);
       } else {
         console.error(`[${xhr.status}] ${file.name} 上传失败`);
       }
@@ -23,43 +24,49 @@ export class PostFile {
 
   static fetch(file: File, url: string, data: object) {
     const formData = new FormData();
+    Object.entries(data)
+      .forEach(([k, v]) => {
+        formData.append(k, v);
+      });
     formData.append('file', file);
-    Object.entries(data).forEach(([k, v]) => {
-      formData.append(k, v);
-    });
 
     fetch(url, {
       method: 'POST',
       body: formData,
     })
       .then((response) => {
-        console.log(`${file.name} 上传成功`, response);
+        if (response.status === 204 || response.status === 204) {
+          console.info(`${file.name} 上传成功`, response);
+        } else {
+          console.error(`${file.name} 上传失败`, response);
+        }
       })
       .catch((error) => {
-        console.error(`${file.name} 上传失败`, error);
+        console.error(`${file.name} 上传异常`, error);
       });
   }
 
   static axios(file: File, url: string, data: object) {
     const formData = new FormData();
+    Object.entries(data)
+      .forEach(([k, v]) => {
+        formData.append(k, v);
+      });
     formData.append('file', file);
-    Object.entries(data).forEach(([k, v]) => {
-      formData.append(k, v);
-    });
 
     axios.post(
       url,
-      formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      })
+      formData
+    )
       .then(function (response) {
-        console.log(`${file.name} 上传成功`, response);
+        if (response.status === 204 || response.status === 204) {
+          console.info(`${file.name} 上传成功`, response);
+        } else {
+          console.error(`${file.name} 上传失败`, response);
+        }
       })
       .catch(function (error) {
-        console.error(`${file.name} 上传失败`, error);
+        console.error(`${file.name} 上传异常`, error);
       });
   }
 }
@@ -76,7 +83,7 @@ export function retrievePostUrl(file: File, cb: (file: File, url: string, data: 
 }
 
 export function xhrPostFile(file?: File) {
-  console.log('xhrPostFile', file);
+  console.log('xhr.PostFile', file);
   if (file) {
     retrievePostUrl(file, (file: File, url: string, data: object) => {
       PostFile.xhr(file, url, data);
@@ -85,7 +92,7 @@ export function xhrPostFile(file?: File) {
 }
 
 export function fetchPostFile(file?: File) {
-  console.log('fetchPostFile', file);
+  console.log('Fetch.PostFile', file);
   if (file) {
     retrievePostUrl(file, (file: File, url: string, data: object) => {
       PostFile.fetch(file, url, data);
@@ -94,7 +101,7 @@ export function fetchPostFile(file?: File) {
 }
 
 export function axiosPostFile(file?: File) {
-  console.log('axiosPostFile', file);
+  console.log('Axios.PostFile', file);
   if (file) {
     retrievePostUrl(file, (file: File, url: string, data: object) => {
       PostFile.axios(file, url, data);
